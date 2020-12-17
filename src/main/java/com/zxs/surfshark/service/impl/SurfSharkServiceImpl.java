45d5af3d9c6c00b9f90dc4ac968efd7536856c77
zouxianshi <1813561379@qq.com>
2020-12-18 02:14:17 +0800

    111

--- src/main/java/com/zxs/surfshark/service/impl/SurfSharkServiceImpl.java
+++ src/main/java/com/zxs/surfshark/service/impl/SurfSharkServiceImpl.java
@@ -1,10 +1,14 @@
 package com.zxs.surfshark.service.impl;
 
 import com.zxs.surfshark.beans.SurfSharkEntity;
+import com.zxs.surfshark.dao.SurfSharkEntityDAO;
 import com.zxs.surfshark.service.SurfSharkService;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Service;
 
 @Service("surf")
+@Mapper
 public class SurfSharkServiceImpl implements SurfSharkService {
     @Override
     public void insertSurfshark(SurfSharkEntity surfSharkEntity) {
